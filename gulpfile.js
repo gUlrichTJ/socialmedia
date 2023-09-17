410b0fe65333200a6c7deb7906d03a0682003c45
gUlrichTJ <gracianomathey@gmail.com>
2023-09-17 20:26:14 +0000

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,10 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
-const browsersync = require('browser-sync');
+const browsersync = require('browser-sync').create();
 
 // Use dart-sass for @use
 sass.compiler = require('dart-sass');
@@ -17,7 +16,7 @@ function scssTask() {
     return src('app/scss.style.scss', { sourcemaps: true})
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(dest('dist', {sourcemaps : '-' }))
+        .pipe(dest('dist', {sourcemaps : '.' }))
 }
 
 // JavaSript Task
@@ -25,5 +24,33 @@ function jsTask() {
     return src('app/js/script.js', {sourcemaps: true })
         .pipe(babel({ presets: ['@babel/preset-env'] }))
         .pipe(terser())
-        .pipe(dest('dist', { sourcemaps: '-' }))
+        .pipe(dest('dist', { sourcemaps: '.' }))
 }
+
+// Browsersync
+function browserSyncServe(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browserSyncReload);
+    watch(
+        ['app/scss/**/*.scss', 'app/**/*.js'],
+        series(scssTask, jsTask, browerSyncReload)
+    );
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
